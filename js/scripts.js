80d2f564077fc8754b23347e79e6b149b6f2c4aa
Anna Timofeeva and Emilie Thoreson <anna@>
2017-06-14 13:47:51 -0700

    display round tally correctly

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ function PlayerOne(name) {
 
 function PlayerTwo(name) {
   this.name = name;
-  this.roundtally = roundtally;
-  this.total = total;
+  this.roundtally = 0;
+  this.total = 0;
 }
 
 // dice:
@@ -18,30 +18,16 @@ function oneThroughSix(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-PlayerOne.prototype.setToZero = function {
+
+PlayerOne.prototype.addRound = function(number) {
   this.roundtally === 0;
-}
-
-
-
-PlayerOne.prototype.addToRoundTally = function(number) {
-    console.log(this.roundtally);
-    while (number != 1) {
-      this.roundtally += number;
-      console.log(this.roundtally);
-      return this.roundtally;
-    }
-}
-
-function isItOne(number) {
   if (number === 1) {
-    this.roundtally === 0;
-    return false;
+    return 0;
   } else {
-    return true;
+    this.roundtally += number;
+    return this.roundtally;
   }
 }
-
 
 
 // user interface logic
@@ -52,6 +38,22 @@ $(document).ready(function() {
     var playerOneName = $("input#player1name").val();
     $("p#1name").append("<p>Name: " + playerOneName + "</p>")
     var playerOne = new PlayerOne(playerOneName);
+    //Player 1 clicks 'roll':
+      $("button#rollbutton1").click(function(event) {
+        $(".rolled-dice1").empty();
+        $(".rolled-dice1").append("<h3>You rolled a: </h3>")
+        var thisClick = parseInt(oneThroughSix(1,6));
+        $(".rolled-dice1").append("<h4>" + thisClick + "</h4>");
+        var roll = playerOne.addRound(thisClick);
+        if (roll === 0) {
+          $("#1roundtally").empty();
+          $("#1roundtally").append("<h5> Round Tally: " + roll + "</h5>")
+          $(".rolled-dice1").append("<h5>Too bad! You lose your points. Next player's turn.</h5>");
+        } else {
+          $("#1roundtally").empty();
+          $("#1roundtally").append("<h5> Round Tally: " + roll + "</h5>")
+        };
+      });
   });
 
 //Player 2 adds name:
@@ -63,18 +65,7 @@ $(document).ready(function() {
     var playerTwo = new PlayerTwo(playerTwoName);
   });
 
-//Player 1 clicks 'roll':
-  $("button#rollbutton1").click(function(event) {
-    $(".rolled-dice1").empty();
-    $(".rolled-dice1").append("<h3>You rolled a: </h3>")
-    var thisClick = oneThroughSix(1,6);
-    $(".rolled-dice1").append("<h4>" + thisClick + "</h4>");
-    if (isItOne(thisClick) === false) {
-      $(".rolled-dice1").append("<h5>Too bad! You lose your points. Next player's turn.</h5>");
-    var roundTotal = playerOne.addToRoundTally(thisClick);
-    console.log(roundTotal);
-    };
-  });
+
 
 //Player 2 clicks 'roll':
   $("button#rollbutton2").click(function(event) {
@@ -82,8 +73,11 @@ $(document).ready(function() {
     $(".rolled-dice2").append("<h3>You rolled a: </h3>")
     var thisClick = oneThroughSix(1,6);
     $(".rolled-dice2").append("<h4>" + thisClick + "</h4>");
-    if (isItOne(thisClick) === false) {
+    var roll = playerTwo.addRound(thisClick);
+    if (roll === 0) {
       $(".rolled-dice2").append("<h5>Too bad! You lose your points. Next player's turn.</h5>");
+    } else {
+      $("#2roundtally").append("<h5> Round Tally: " + roll + "</h5>")
     };
   });
 
